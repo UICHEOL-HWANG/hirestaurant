e69ab0d3b7c1dea3f01d9b62f783dd40f866a6cf
UICHEOL-HWANG <icuchoel@gmail.com>
2023-09-11 19:07:36 +0900

    프로필 드롭다운 수정

--- backend/frontend/js/profileMenu.js
+++ backend/frontend/js/profileMenu.js
@@ -1,8 +1,11 @@
 {
+const adminLink = isAdmin ? '<a href="${adminURL}"> 관리자</a>' : '';
 const menu = `
 <div class="profile-menu">
     <a href="${profileURL}">내 프로필</a>
+    ${adminLink}
     <a href="${logoutURL}" class="warn"> 로그아웃</a>
+
 </div>
 `
 tippy('.navbar .cp-avatar',{
