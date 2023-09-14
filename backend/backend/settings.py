2f227e7d6cacb05eb4cce4a99bb98feeaaa2755e
UICHEOL-HWANG <icuchoel@gmail.com>
2023-09-14 18:59:47 +0900

    세팅값 변경

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -95,12 +95,15 @@ pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('SQL_ENGINE','django.db.backends.sqlite3'),
-        'NAME': os.environ.get('SQL_DATABASE',os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER' : os.environ.get('SQL_USER','user'),
-        'PASSWORD' : os.environ.get('SQL_PASSWORD','password'),
-        'HOST' : os.environ.get('SQL_HOST','localhost'),
-        'PORT' : os.environ.get('SQL_PORT','3306'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('SQL_DATABASE', 'hirestaurant.dev'),
+        'USER': os.environ.get('SQL_USER', 'admin'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', '!Ghgh1234'),
+        'HOST': 'db',  # Docker Compose 컨테이너 이름
+        'PORT': os.environ.get('SQL_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
